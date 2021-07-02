04764816fa7f8388486265e3d40e722c0c0e37b3
ajitabh7 <ajitabh7@gmail.com>
2021-07-01 23:59:27 -0500

    fib series

---
+++ fib.py
@@ -0,0 +1,10 @@
+#! /var/cfengine/bin/python
+# simple fibonacci series
+# the sum of two elements defines the next set
+
+a, b = 0, 1
+while b < 100:
+    print(b, end = ' ', flush = True)
+    a, b = b, a + b
+
+print() # line ending
